805bc397dd6e2f2abad6c3c363da388480b6f5e8
KTheES <keunsu2889@gmail.com>
2026-04-14 10:24:35 +0900

    python 0413 study complete

--- Study/week07/week07_dictionary.py
+++ Study/week07/week07_dictionary.py
@@ -7,12 +7,35 @@
 #.keys() -> k들 출력
 #.values() -> v들 출력 
 
-singer ={}
+# singer ={}
 
-singer['이름']= '트와이스'
-singer['구성원 수']= 9
-singer['데뷔']= '서바이벌 식스틴'
-singer['대표곡']= 'signal'
+# singer['이름']= '트와이스'
+# singer['구성원 수']= 9
+# singer['데뷔']= '서바이벌 식스틴'
+# singer['대표곡']= 'signal'
 
-for k in singer.keys():
-    print("%s --> %s"%(k,singer[k]))
+# for k in singer.keys():
+#     print("%s --> %s"%(k,singer[k]))
+# print()
+
+# for v in singer.values():
+#     print(singer)
+# print()
+
+# for i in singer.items():
+#     print(singer)
+
+# 이거 val / item다른점은  시험에 안나옵니다
+
+# 딕셔너리도 정렬 가능합니다 ->operator 모듈 import 필요함 ()
+# import operator
+
+# tL = sorted(trainDic.items(),key = operator.itemgetter(0))
+
+
+
+# 리스트 컴프리헨션
+
+numList = [num for num in range(1,21) if(num%3==0)]
+print(numList)
+
